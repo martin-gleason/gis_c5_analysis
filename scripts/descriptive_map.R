cbbbffc038c09ca68aea557c7ab85960afb5c8c6
Martin Gleason <officergleason@gmail.com>
2020-10-01 14:05:37 -0500

    map version 1

--- scripts/descriptive_map.R
+++ scripts/descriptive_map.R
@@ -2,10 +2,19 @@
 library(tidyverse)
 library(sf)
 library(geojsonsf)
+library(leaflet)
 
-cases <- read_rds("input/juveniles.RDS")
+file <- here("input/juveniles.RDS")
+shapes <- here("shapes/cpd_districts.geojson")
+cases <- read_rds(file)
+cpd_district <- read_sf(shapes, crs = "+proj=longlat +datum=WGS84")
+
+cpd_district <- cpd_district%>%
+  filter(dist_num != 31)
 
 view (cases)
+
+cpd_pal <- colorFactor("viridis", domain = as.factor(cpd_district$dist_num))
 
 #Should make a function -- have it somewhere, actually.
 cases$race <- cases$race %>% str_replace_all("Black Hispanic /Latino", "Black Hispanic/Latino")
@@ -20,4 +29,39 @@ multiple_cases <- cases %>%
   summarise(count = n()) %>% 
   arrange(desc(count))
 
-cpd_district <- read_sf("shapes/cpd_districts.geojson")
+
+case_location <- cases %>%
+  select(client_no, lon, lat) %>%
+  filter(!is.na(lon)) %>%
+  st_as_sf(coords = c("lon", "lat"),
+           crs = 4326)
+
+cpd_district_counts <- cpd_district %>%
+  mutate(counts = st_intersects(., case_location) %>% lengths(),
+         center = st_centroid(.$geometry))
+
+
+base_chicago <- leaflet() %>% 
+  setView(lat = "41.8781", lng = "-87.6298", zoom = 10) %>%
+  addProviderTiles("Stamen.Terrain")
+
+arrest_count <- base_chicago %>%
+  addPolygons(data = cpd_district_counts,
+              group = 1,
+              color = "black",
+              fillColor = ~cpd_pal(cpd_district$dist_num),
+              label = cpd_district$dist_label,
+              fillOpacity = 0.3,
+              weight = 2) %>%
+  addMarkers(data = cpd_district_counts$center,
+             label = paste(cpd_district_counts$dist_label, 
+                           "District has a total of \n", 
+                           cpd_district_counts$counts, "arrests", " "),
+             labelOptions = labelOptions(noHide = TRUE))
+
+class(count)
+ 
+#   addMarkers(lng = cases$lon,
+#              lat = cases$lat,
+#              data = cases)
+#   
