512f9151ae8e9ac3b1b30ce587eaf1abf1d1a251
Martin Gleason <officergleason@gmail.com>
2020-10-01 15:03:56 -0500

    fixed label

--- scripts/descriptive_map.R
+++ scripts/descriptive_map.R
@@ -33,7 +33,7 @@ arrest_count <- base_chicago %>%
               fillOpacity = 0.3,
               weight = 2) %>%
   addMarkers(data = cpd_district_counts$center,
-             label = paste0(cpd_district_counts$dist_label, 
+             label = paste(cpd_district_counts$dist_label, 
                            "district has a total of", 
                            cpd_district_counts$counts, "cases.", sep = " "),
              labelOptions = labelOptions(noHide = TRUE))
